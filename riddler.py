a312b05a2d5f32b4742ee5a01191a782b5b55750
terrencejay <150986847+terrencejay@users.noreply.github.com>
2024-10-18 02:45:28 -0500

    Update riddler.py

--- riddler.py
+++ riddler.py
@@ -3,6 +3,6 @@ import random
 mood = ["Happy", "Sad", "Angry","Calm","relaxed","anxious","Depressed"]
 days =["Monday","Tuesday","Wedenesday","Thursday","Friday","Saturday","Sunday"]
 
-for day in days:
+for i in range(len(days)):
     feelings = random.choice(mood)
-    print (f"On {day} you were feelings {feelings}")
+    print (f"On {days[i]} you were feelings {feelings}")
